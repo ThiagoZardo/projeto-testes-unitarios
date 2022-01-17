76f460bcc9c843b1d259ddb273f1d3fa733180bf
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-01-17 18:35:44 -0300

    Requisito 03

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,21 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (nome, idade) => {
+  let nomes = nome;
+  let idades = idade;
+  const frase = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
+  if ((typeof nomes === 'undefined') || (typeof idades === 'undefined')) {
+    return undefined;
+  }
+
+  return frase;
+};
+
+console.log(vqv('Thiago', 29));
 
 module.exports = vqv;
