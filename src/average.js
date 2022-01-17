0c5c51ef9fc212f8ba30af88e395d63148260018
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-01-17 17:20:27 -0300

    Requisito 01

--- src/average.js
+++ src/average.js
@@ -12,8 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  let soma = 0;
+  const valores = [];
+  let media = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    valores.push(array[i]);
+    soma += Math.round(valores[i]);
+    if ((typeof valores[i] !== 'number')) {
+      return undefined;
+    }
+  }
+  if (array.length === 0) {
+    return undefined;
+  }
+    media = Math.round(soma / array.length);
+    return media;
 };
-
 module.exports = average;
