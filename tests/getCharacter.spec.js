a6213e6f62db2a158bbe4425d53f8528e75e6029
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-01-22 23:09:26 -0300

    Requisito 09

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,13 +43,28 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter('')).toBeFalsy();
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    const retornoArya = getCharacter('Arya');
+    expect(retornoArya.name === 'Arya Stark' && retornoArya.class === 'Rogue' && retornoArya.phrases[0] === 'Not today' && retornoArya.phrases[1] === 'A girl has no name.').toBeTruthy();
+
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    const retornoBrienne = getCharacter('Brienne');
+    expect(retornoBrienne.name === 'Brienne Tarth' && retornoBrienne.class === 'Brienne Tarth' && retornoBrienne.phrases[0] === 'Im No Lady, Your Grace.' && retornoBrienne.phrases[1] === 'I, Brienne Of Tarth, Sentence You To Die.');
+
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    const retornoMelissandre = getCharacter('Melissandre');
+    expect(retornoMelissandre.name === 'Melissandre' && retornoMelissandre.class === 'Necromancer' && retornoMelissandre.phrases[0] === 'Death By Fire Is The Purest Death.' && retornoMelissandre.phrases[1] === 'For The Night Is Dark And Full Of Terrors.');
+
     // Teste se os parâmetros não são Case Sensitive.
+    const retornoSensitive = getCharacter();
+    expect(getCharacter('AryA')).toEqual({"class": "Rogue", "name": "Arya Stark", "phrases": ["Not today", "A girl has no name."]}); 
+
+    expect(getCharacter('ArYa').name).toBe('Arya Stark');
+
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('Thiago')).toBe(undefined);
   });
 });
